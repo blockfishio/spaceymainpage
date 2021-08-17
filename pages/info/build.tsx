66ec874ae315a17f947cfc0af9b8eb2005f1f815
shakurapanda <sean.zheng.xiao@gmail.com>
2021-08-17 14:53:48 -0400

    escape quote

--- pages/info/build.tsx
+++ pages/info/build.tsx
@@ -17,7 +17,7 @@ const Info: NextPage = () => {
                     <div>
                     The shoes had been there for as long as anyone could remember. 
                     In fact, it was difficult for anyone to come up with a date they had first appeared. 
-                    It had seemed they'd always been there and yet they seemed so out of place.
+                    It had seemed they&apos;d always been there and yet they seemed so out of place.
                      Why nobody had removed them was a question that had been asked time and again, 
                      but while they all thought it, nobody had ever found the energy to actually do it. 
                      So, the shoes remained on the steps, out of place in one sense,
