5717628523bdae4791e3a7ec906274c6a40dc2d9
shakurapanda <sean.zheng.xiao@gmail.com>
2021-08-25 14:23:55 -0400

    Canvans Image Generator

---
+++ components/freetickets/ImageCanvas.tsx
@@ -0,0 +1,56 @@
+
+
+import React, {createRef, useEffect, useRef, useState} from "react";
+
+interface Props {
+    title: string
+}
+
+const Canvas = ({title}: Props) => {
+
+    const canvasObj = createRef<HTMLCanvasElement>()
+    const [imageUrl, setImage] =  useState("")
+
+    useEffect(() => {
+  
+        if (!canvasObj.current)
+          return 
+        else {
+         const canvasObject = canvasObj.current
+         const ctx = canvasObj.current.getContext("2d")
+         if (ctx) {
+
+            const image = new Image()
+            image.src = "../../images/ic_ticket@2x.png"
+
+            image.onload = function() {
+                canvasObject.width=image.width
+                canvasObject.height=image.height
+
+                ctx.drawImage(image, 0, 0, image.width, image.height)
+                ctx.font= image.width / 35 + "px sans-serif"
+                ctx.fillStyle = "white";
+                ctx.textAlign = "center";
+                
+                ctx.fillText(title.toUpperCase(), image.width / 3 + title.length * 5 + 10, image.height / 3)
+                //ctx.drawImage(newone,width*0.35,height*0.75,100,100)
+                if (canvasObj.current) {
+                    const url=canvasObj.current.toDataURL()
+                    setImage(url)
+                }
+            }           
+            // console.log(text)
+         }
+        }
+       }
+    )
+
+    
+    return (
+        <div className="mx-auto">
+           <canvas ref={canvasObj} className="w-4/5 mx-auto" />
+        </div>
+    )
+}
+
+export default Canvas
--- pages/freeticket/index.tsx
+++ pages/freeticket/index.tsx
@@ -1,9 +1,15 @@
+
+import {useState, useRef} from 'react'
 import type { NextPage } from 'next'
-import Image from 'next/image'
 import withLayout from '../../hocs/withLayout'
-import ticketImage from '../../public/images/ic_ticket@2x.png'
+import ImageCanvas from '../../components/freetickets/ImageCanvas'
 
 const FreeTicket: NextPage = () => {
+
+  const [name, setName] = useState("")
+  const inputBox = useRef<HTMLInputElement>(null)
+  const emailBox = useRef<HTMLInputElement>(null)
+
   return (
     <div className="flex-row flex mt-32 gap-x-6">
        <div className="flex flex-col items-center">
@@ -22,25 +28,43 @@ const FreeTicket: NextPage = () => {
               </div>
                <div className="mt-10 ">
                    <div className="font-bankgothic bg-spacey-form-background p-10 overflow-hidden text-xl">
-                        <form>
+                       
                         <div className="mb-3"> 
-                           <input placeholder="NAME" className="rounded pl-4 py-2 bg-spacey-form-input-background text-spacey-form-text"/>
+                           <input ref={inputBox} placeholder="NAME" className="rounded pl-4 py-2 bg-spacey-form-input-background text-spacey-form-text"/>
                         </div>
                         <div className="mb-8">  
-                            <input placeholder="EMAIL"  className="rounded pl-4 py-2 bg-spacey-form-input-background text-spacey-form-text"/>
+                            <input placeholder="EMAIL" ref={emailBox} className="rounded pl-4 py-2 bg-spacey-form-input-background text-spacey-form-text"/>
                         </div>
                         <div >  
-                           <button className=" rounded w-full pl-4 py-2 border-spacey-form-border border-2 text-spacey-form-text">Claim your free ticket</button>
+                           <button className=" rounded w-full pl-4 py-2 border-spacey-form-border border-2 text-spacey-form-text"
+                                   onClick={() => {
+                                       if(inputBox.current){
+                                        setName(inputBox.current.value)
+                                        inputBox.current.value = ""
+
+                                        if (emailBox.current) {
+                                            emailBox.current.value = ""
+                                        }
+                                       }
+                                       else 
+                                         setName("")
+                                        
+                                       return false
+                                   }}
+                           >
+                               Claim your free ticket
+                            </button>
                         </div>
-                        </form>
                     </div>
               </div>
         </div>
-        <div className="bg-astro-background flex-1 bg-cover flex flex-col items-center justify-center">
-            <div className="m-5">
-           <Image src={ticketImage} />
+        <div className="bg-astro-background flex-1 bg-cover grid grid-cols-1 items-center justify-center">
+           
+           <div className="m-5 justify-self-stretch ">
+              <ImageCanvas title={name} />
            </div>
         </div>
+        
     </div>
   )
 }
