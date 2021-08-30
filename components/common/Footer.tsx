759dcca4118fafb0d0a1ddfe5e5eb441d042c6b4
shakurapanda <sean.zheng.xiao@gmail.com>
2021-08-30 14:07:22 -0400

    update footer

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaFacebookF } from "react-icons/fa"
+import { FaFacebookF, FaDiscord, FaTelegramPlane, FaMediumM, FaTwitter} from "react-icons/fa"
 
 import Link from 'next/link'
 interface Props {
@@ -66,7 +66,32 @@ const Footer = ({mode}:Props) => {
                 </ul>
             </div>
             <div className={"flex-initial flex items-center space-x-4 mx-auto " + (mode === 'dark' ? 'text-spacey-light-grey': '')}>
-                <div className="height-2"><FaFacebookF /></div>
+            <div className="height-2 cursor-pointer" >
+                    <Link href="https://discord.com/invite/cUeNS8UzGW">
+                          <FaDiscord />
+                    </Link>
+                </div>
+                <div className="height-2 cursor-pointer" >
+                    <Link href="hhttps://t.me/Spacey2025">
+                          <FaTelegramPlane />
+                    </Link>
+                </div>
+                <div className="height-2 cursor-pointer" >
+                    <Link href="https://medium.com/@spacey2025">
+                          <FaMediumM />
+                    </Link>
+                </div>
+                <div className="height-2 cursor-pointer" >
+                    <Link href="https://www.facebook.com/SpaceY-2025-102636005328431">
+                          < FaTwitter />
+                    </Link>
+                </div>
+             
+                <div className="height-2 cursor-pointer" >
+                    <Link href="https://twitter.com/spacey2025">
+                          <FaFacebookF />
+                    </Link>
+                </div>
                 <div className=" height-2 align-text-top"><span>@2021 space2025.com</span></div>
             </div>
           </div>
