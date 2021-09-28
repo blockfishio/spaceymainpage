4d87c56d56621eaf4cbf91a47b86318c16cd0e46
shakurapanda <sean.zheng.xiao@gmail.com>
2021-09-28 12:01:40 -0400

    update market place

--- pages/info/market.tsx
+++ pages/info/market.tsx
@@ -3,120 +3,37 @@ import Image from 'next/image'
 import Link from 'next/link'
 import SideMenu from '../../components/info/SideMenu'
 import withLayout from '../../hocs/withLayout'
-import buildImage from '../../public/images/ic_build@2x.png'
-import fightImage from '../../public/images/ic_fight@2x.png'
-import tradeImage from '../../public/images/ic_trade_info@2x.png'
-import upgradeImage from '../../public/images/ic_upgrade@2x.png'
-import exploreSmall from '../../public/images/explore_small.png'
-import infoImage1 from '../../public/images/mobile_info_1.png'
-import infoImage2 from '../../public/images/mobile_info_2.png'
-import infoImage3 from '../../public/images/mobile_info_3.png'
-import infoImage4 from '../../public/images/mobile_info_4.png'
+import marketPlaceImage from '../../public/images/marketplace.png'
 
 const Info: NextPage = () => {
   return (
     <div>
-      <div className="grid grid-cols-1 md:grid-cols-4 container mx-auto mt-10  md:mt-28 ">
-          <SideMenu active="market" />
-          <div className="md:col-span-3 hidden md:block">
-                <div className="flex flex-row gap-4 justify-between">
-                    
-                  <div className="">
-                      <Image src={buildImage} />
-                  </div>
-
+    <div className="grid md:grid-cols-4 container mx-auto mt-10 md:mt-28">
+        <SideMenu active="build" toggle={false} />
+        <div className="col-span-3 p-5 md:p-0">
+              <div className="flex flex-col gap-4 justify-between">
+                  <h3 className=" text-4xl py-4 font-bankgothic">The Marketplace </h3>
                   <div>
-                      <Image src={fightImage} />
-                  </div>
+                     <div>
+                       Select <b>Market</b> to see all the items that are on Sale.
 
-                  <div>
-                      <Image src={upgradeImage} />
-                  </div>
-
-                  <div>
-                      <Image src={tradeImage} />
-                  </div>
-
-                </div>
-          </div>
-      </div>
-      {/** desktop version  */}
-      <div className="hidden md:block">
-        <div className="gradientBackgroundLx mt-10 md:mt-20">
-          <div className="container mx-auto grid grid-cols-1 md:grid-cols-4">
-              <div>
-
+                        <ul className="list-disc pl-5">
+                          <li className="mt-5 mb-5">Select the <b>Category</b> to view only a specific type of item .</li>
+                          <li className="mb-5"><b>Order</b> them by different criteria like recently listed, cheapest, etc.</li>
+                          <li className="mb-5">Toggle <b>On sale</b> off to view items that aren’t for sale.</li>
+                          <li className="mb-5"><b>Filter</b> items by name to find something specific.</li>
+                        </ul>
+                     </div>
+                   </div>
               </div>
-              <div className="md:col-span-3">
-              <h3 className="font-bankgothic text-4xl py-4"> SpaceY2025 Market</h3>
+              <div className="block mt-10">
+                  <Image src={marketPlaceImage} layout="responsive" />
               </div>
-          </div>
-        </div>
-        <div>
-          <div className="container mx-auto grid grid-cols-1 md:grid-cols-4 mt-10">
-              <div>
-
-              </div>
-              <div className="md:col-span-3">
-              A maze tower defense game combined with open-world RPG features built on the Blockchain. Use NFTs to develop and defend the human base on Mars. While overcoming unknown challenges, revealing the secret of our universe.
-By contributing to space exploration, you will be rewarded with tokens as the in-game currency.
-Earth is the cradle of human civilization, and the cradle is always fragile. For the continuation of human civilization, we must plan ahead. Mankind is bound to step out of the cradle to continue to evolve. Diving into the deep space of the universe is a problem we will face sooner or later. 
-Come join us as one of the human pioneers.
- {/* <div className="float-right pl-2 pt-5" >
-                        <Image src={exploreSmall}  className="float-right" width="400" height="350"/>
-                        </div> */}
-                
-                  </div>
-          </div>
-        </div>
-      </div>
-
-        {/** mobile version  */}
-        <div className="flex flex-col md:hidden gap-y-10 justify-center mt-20 ">
-           <div className="font-bankgothic text-3xl text-center tracking-wide">
-              SPACEY 2025 INFORMATION 
-           </div>
-           <div className="opacity-90 text-center px-5">
-                Find help about the various topics in each of these sections
-           </div>
-           <div>
-             <div className="grid grid-cols-2 gap-5 px-5">
-               <div >
-                 <Link href="/info">
-                  <Image className="rounded-xl" src={infoImage1} layout="responsive" />
-                  </Link>
-
-                  <div className="opacity-90 mt-4">Home</div>
-                  <div className="opacity-70 text-sm">General info for players</div>
-               </div>
-               <div>
-               <Link href="/info">
-                <Image className="rounded-xl" src={infoImage2} layout="responsive"/>
-               </Link>
-               <div className="opacity-90 mt-4">Background Story</div>
-                  <div  className="opacity-70 text-sm">Read interesting stories behind this game.</div>
-               </div>
-             </div>
-             <div className="grid grid-cols-2 mt-10  gap-5 px-5">
-              <div>
-              <Link href="/info/build">
-                 <Image className="rounded-xl" src={infoImage3} layout="responsive"/>
-              </Link>
-                  <div className="opacity-90 mt-4">Gameplay</div>
-                  <div  className="opacity-70 text-sm">Learn some basic playing techniques</div>
-              </div>
-              <div>
-              <Link href="/info">
-                <Image className="rounded-xl" src={infoImage4} layout="responsive"/>
-              </Link>
-                  <div className="opacity-90 mt-4" >Trade</div>
-                  <div className="opacity-70 text-sm">Learn how to trade exclusive tokens.</div>
-              </div>
-             </div>
-           </div>
         </div>
     </div>
-  
+
+
+  </div>
   )
 }
 
